5ba471a0d0583147e30d5862410c795ee22d2d63
Azrael15158 <96781294+Azrael15158@users.noreply.github.com>
2021-12-28 20:14:47 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace lab3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var studList = new StudCont();
+            studList.Add(new Student("Луговой Александр Евгениевич", new DateTime(2002, 4, 2), new DateTime(2019, 9, 1), "Б", 119, "КИТ", "Компьютерные игры", 95));
+            studList.Add(new Student("Мыглин Сергей Александрович", new DateTime(2004, 11, 30), new DateTime(2019, 9, 1), "Б", 119, "КИТ", "Компьютерные игры", 80));
+            studList.Add(new Student("Мыглин Андрей Александрович", new DateTime(2002, 3, 31), new DateTime(2019, 9, 1), "Б", 119, "КИТ", "Компьютерные игры", 67));
+            studList.Add(new Student("Хаврюта Андрей Юрьевич", new DateTime(2002, 3, 10), new DateTime(2019, 9, 1), "В", 419, "КИТ", "АТМ", 75));
+            studList.Add(new Student("Васильев Даниил Анатолиевич", new DateTime(2002, 12, 24), new DateTime(2019, 9, 1), "В", 419, "КИТ", "АТМ", 71));
+            StudContHelper helper = new StudContHelper(studList);
+            helper.Write();
+            var studList2 = helper.Read();
+            studList2.PrintAll(false);
+            var name = "Мыглин Сергей Александрович";
+            var stud = studList2.GetStudent(name);
+            stud.Performance += 5;
+            studList2.ChangeStudent(name, stud);
+            studList2.PrintAll(false);
+            //File.WriteAllText("C:\\Users\\Golden\\source\\repos\\lab3\\output.txt", stud.toString() + "\n");
+        }
+    }
+}
